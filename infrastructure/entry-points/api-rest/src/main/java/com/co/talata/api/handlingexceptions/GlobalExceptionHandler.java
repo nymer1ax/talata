ef3db63b1274257dc026c2608fe981fd7135410e
Ralph Sliger <>
2022-11-26 13:13:27 -0500

    Add: exceptiones personalizadas

--- infrastructure/entry-points/api-rest/src/main/java/com/co/talata/api/handlingexceptions/GlobalExceptionHandler.java
+++ infrastructure/entry-points/api-rest/src/main/java/com/co/talata/api/handlingexceptions/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.co.talata.api.handlingexceptions;
 
 import com.co.talata.usecase.exceptions.Response;
+import com.co.talata.usecase.exceptions.custom.CustomException;
 import com.co.talata.usecase.exceptions.custom.MovieNotFoundException;
+import com.co.talata.usecase.exceptions.custom.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,7 +11,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.Collections;
 
 @ControllerAdvice
@@ -26,6 +27,43 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .build();
 
         return new ResponseEntity<Response>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<Response> unauthorized(UnauthorizedException ex){
+
+        Response response = Response.builder()
+                .fecha(LocalDateTime.now())
+                .codigoResultado("UNAUTHORIZED")
+                .descripcionRespuesta(ex.getMessage())
+                .result(Collections.emptyList())
+                .build();
+
+        return new ResponseEntity<Response>(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Response> custom(CustomException ex){
+        Response response = Response.builder()
+                .fecha(LocalDateTime.now())
+                .codigoResultado("BAD_REQUEST")
+                .descripcionRespuesta(ex.getMessage())
+                .result(Collections.emptyList())
+                .build();
+
+        return new ResponseEntity<Response>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Response> custom(RuntimeException ex){
+        Response response = Response.builder()
+                .fecha(LocalDateTime.now())
+                .codigoResultado("BAD_REQUEST")
+                .descripcionRespuesta(ex.getMessage())
+                .result(Collections.emptyList())
+                .build();
+
+        return new ResponseEntity<Response>(response, HttpStatus.BAD_REQUEST);
     }
 
 }
